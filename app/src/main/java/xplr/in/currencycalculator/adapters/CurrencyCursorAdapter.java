3c7f7d84594b2d3de333f9961a594d8358cf36b6
Christopher Heriot <cheriot@gmail.com>
2016-05-19 01:22:34 +0800

    Use new ViewHolder for CurrencyCursorAdapter.

--- app/src/main/java/xplr/in/currencycalculator/adapters/CurrencyCursorAdapter.java
+++ app/src/main/java/xplr/in/currencycalculator/adapters/CurrencyCursorAdapter.java
@@ -1,26 +1,16 @@
 package xplr.in.currencycalculator.adapters;
 
-import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.CursorAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.yahoo.squidb.data.SquidCursor;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import xplr.in.currencycalculator.R;
 import xplr.in.currencycalculator.activities.CurrencyListActivity;
 import xplr.in.currencycalculator.models.Currency;
-import xplr.in.currencycalculator.models.CurrencyMeta;
 import xplr.in.currencycalculator.models.SelectedCurrency;
 import xplr.in.currencycalculator.repositories.CurrencyMetaRepository;
 
@@ -50,7 +40,7 @@ public class CurrencyCursorAdapter extends CursorAdapter {
         View view = LayoutInflater
                 .from(context)
                 .inflate(listItemLayout, parent, false);
-        view.setTag(new ViewHolder(view));
+        view.setTag(new SelectableCurrencyViewHolder(view));
         return view;
     }
 
@@ -65,36 +55,7 @@ public class CurrencyCursorAdapter extends CursorAdapter {
         SelectedCurrency currency = new SelectedCurrency();
         currency.readPropertiesFromCursor((SquidCursor)cursor);
 
-        ViewHolder viewHolder = (ViewHolder) view.getTag();
-
-        CurrencyMeta meta = currencyMetaRepository.findByCode(currency.getCode());
-
-        if(viewHolder.codeText != null) viewHolder.codeText.setText(currency.getCode());
-        if(viewHolder.nameText != null) viewHolder.nameText.setText(currency.getName());
-        if(viewHolder.checkBox != null) viewHolder.checkBox.setChecked(currency.isSelected());
-
-        SelectedCurrency baseCurrency = this.currencyListActivity.getBaseCurrency();
-        if(viewHolder.rateText != null) {
-            currency.convertFrom(baseCurrency);
-            viewHolder.rateText.setText(currency.format());
-        }
-
-        if(viewHolder.flagImage != null && meta != null) {
-            Activity activity = (Activity)currencyListActivity;
-            Drawable drawable = activity.getResources().getDrawable(meta.getFlagResourceId(CurrencyMeta.FlagSize.SQUARE));
-            viewHolder.flagImage.setImageDrawable(drawable);
-        }
-    }
-
-    static class ViewHolder {
-        @Bind(R.id.currency_name) TextView nameText;
-        @Nullable @Bind(R.id.currency_code) TextView codeText;
-        @Nullable @Bind(R.id.currency_rate) TextView rateText;
-        @Nullable @Bind(R.id.currency_selected) CheckBox checkBox;
-        @Nullable @Bind(R.id.currency_flag) ImageView flagImage;
-
-        public ViewHolder(View view) {
-            ButterKnife.bind(this, view);
-        }
+        SelectableCurrencyViewHolder viewHolder = (SelectableCurrencyViewHolder) view.getTag();
+        viewHolder.bindView(currency);
     }
 }
