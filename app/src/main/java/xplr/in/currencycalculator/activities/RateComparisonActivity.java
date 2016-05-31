8c57a7423c14bd06c94c50493b9a9968eee6e80a
Christopher Heriot <cheriot@gmail.com>
2016-05-31 12:21:45 +0800

    Radio hide/show of the rate screen forms.

--- app/src/main/java/xplr/in/currencycalculator/activities/RateComparisonActivity.java
+++ app/src/main/java/xplr/in/currencycalculator/activities/RateComparisonActivity.java
@@ -3,6 +3,8 @@ package xplr.in.currencycalculator.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,7 +15,10 @@ import xplr.in.currencycalculator.R;
  */
 public class RateComparisonActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = RateComparisonActivity.class.getSimpleName();
     @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.rate_form) View rateForm;
+    @Bind(R.id.trade_form) View tradeForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +40,17 @@ public class RateComparisonActivity extends AppCompatActivity {
         // Better display:
         // Use material guidelines to show example text while typing (market exchange rate)
         //
+    }
+
+    public void setFeesYes(View view) {
+        Log.v(LOG_TAG, "setFeesYes");
+        rateForm.setVisibility(View.GONE);
+        tradeForm.setVisibility(View.VISIBLE);
+    }
+
+    public void setFeesNo(View view) {
+        Log.v(LOG_TAG, "setFeesNo");
+        rateForm.setVisibility(View.VISIBLE);
+        tradeForm.setVisibility(View.GONE);
     }
 }
